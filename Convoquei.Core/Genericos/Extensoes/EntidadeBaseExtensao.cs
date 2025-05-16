f8463bec6e75dae7b477ef782d60b5688de6f0f6
Ronaldo Bueno <ronaldobueno2002@gmail.com>
2025-05-16 12:51:59 -0300

    Adiciona metodo de extensao para validar entidade

---
+++ Convoquei.Core/Genericos/Extensoes/EntidadeBaseExtensao.cs
@@ -0,0 +1,14 @@
+﻿using Convoquei.Core.Genericos.Entidades;
+using Convoquei.Core.Genericos.Excecoes;
+
+namespace Convoquei.Core.Genericos.Extensoes
+{
+    public static class EntidadeBaseExtensao
+    {
+        public static void LancarRegraDeNegocioExcecaoSe<TEntidade>(this TEntidade entidade, Func<TEntidade, bool> condicao, string mensagem) where TEntidade : EntidadeBase
+        {
+            if (condicao(entidade))
+                throw new RegraDeNegocioExcecao(mensagem);
+        }
+    }
+}
