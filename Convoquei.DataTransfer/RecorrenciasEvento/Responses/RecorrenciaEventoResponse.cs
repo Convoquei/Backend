35b215e76c21396e66861b7b59c4cec4acd75852
Ronaldo Bueno <ronaldobueno2002@gmail.com>
2025-05-21 17:15:40 -0300

    Implementa DTO para para recorrenicia de evento

---
+++ Convoquei.DataTransfer/RecorrenciasEvento/Responses/RecorrenciaEventoResponse.cs
@@ -0,0 +1,26 @@
+﻿using Convoquei.Core.Recorrencias.Entidades;
+
+namespace Convoquei.DataTransfer.RecorrenciasEvento.Responses
+{
+    public record RecorrenciaEventoResponse(
+        Guid Id,
+        string Nome,
+        string Descricao,
+        string Local,
+        string Recorrencia,
+        TimeSpan HoraInicio
+    )
+    {
+        public static explicit operator RecorrenciaEventoResponse(RecorrenciaEventoBase recorrencia)
+        {
+            return new RecorrenciaEventoResponse(
+                recorrencia.Id,
+                recorrencia.Nome,
+                recorrencia.Descricao,
+                recorrencia.Local,
+                recorrencia.DescricaoRecorrencia,
+                recorrencia.DataHoraInicio.TimeOfDay
+            );
+        }
+    }
+}
